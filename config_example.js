989c06f48015b8c2540ce32cee9ba2599ecc41f2
Lilithid <koriio_o@hotmail.com>
2021-06-09 13:55:40 +0200

    examples

---
+++ config_example.js
@@ -0,0 +1,160 @@
+module.exports = {
+	token: '',
+	status: '',
+	fungalcavern: {
+		prefix: '*',
+		id: '',
+		modlogs: '',
+		modmail: '',
+		verilogs: '',
+		rlcommands: '',
+		afkchecks: '',
+		vetRlCommands: '',
+		vetAfkchecks: '',
+		eventRlCommands: '',
+		eventAfkchecks: '',
+		raiderRole: '', // Raider Role
+		vetRaiderRole: '', // Veteran Raider Role
+		eventRaiderRole: '', // Event Raider Role
+		tempKeyRole: '', // Temporary Key Role
+		mutedRole: '', // Muted Role
+		pSuspendedRole: '', // Permanently Suspended Role
+		suspendedRole: '', // Temporary Suspended Role
+		nitroRole: '', // Nitro role
+		patreonRole: '', // Patreon role
+		suspensionLog: '',
+		raidingChannelsAmount: '',
+		vetRaidingChannelsAmount: '',
+		eventRaidingChannelsAmount: '',
+		vc: [
+			{
+				name: 'Lounge',
+				id: ''
+			},
+			{
+				name: 'Raiding 1',
+				id: ''
+			},
+			{
+				name: 'Raiding 2',
+				id: ''
+			},
+			{
+				name: 'Raiding 3',
+				id: ''
+			},
+			{
+				name: 'Raiding 4',
+				id: ''
+			},
+			{
+				name: 'Raiding 5',
+				id: ''
+			},
+			{
+				name: 'Raiding 6',
+				id: ''
+			},
+		],
+		vetVc: [
+			{
+				name: 'Veteran Lounge',
+				id: ''
+			},
+			{
+				name: 'Veteran Raiding 1',
+				id: ''
+			},
+			{
+				name: 'Veteran Raiding 2',
+				id: ''
+			},
+			{
+				name: 'Veteran Raiding 3',
+				id: ''
+			},
+			{
+				name: 'Veteran Raiding 4',
+				id: ''
+			},
+		],
+		eventVc: [
+			{
+				name: 'Lounge',
+				id: ''
+			},
+			{
+				name: 'Event 1',
+				id: ''
+			},
+			{
+				name: 'Event 2',
+				id: ''
+			},
+			{
+				name: 'Event 3',
+				id: ''
+			},
+			{
+				name: 'Realm Clearing',
+				id: ''
+			}
+		]
+	},
+	shattersHcReactions: [
+		'', // portal
+		'', // key
+		'', // warrior
+		'', // paladin
+		'', // priest
+		'', // knight
+		'', // armorbreak
+		'', // mystic
+		'', // assassin
+		'', // first
+		'', // second
+		'', // secret
+	],
+	voidHcReactions: [
+		'', // portal
+		'', // key
+		'', // vial
+		'', // warrior
+		'', // paladin
+		'', // knight
+		'', // mseal
+		'', // fungaltome
+	],
+	cultHcReactions: [
+		'', // portal
+		'', // key
+		'', // warrior
+		'', // paladin
+		'', // knight
+		'', // priest
+		'', // rusher
+	],
+	fungalHcReactions: [
+		'', // portal
+		'', // key
+		'', // warrior
+		'', // paladin
+		'', // priest
+		'', // bard
+		'', // fungaltome
+		'', // knight
+		'', // armorbreak
+		'', // mseal
+		'', // qot
+		'', // slow
+		'', // trickster
+		'', // mystic
+		'', // rusher
+	],
+	eventHcReactions: [
+		'', // warrior
+		'', // paladin
+		'', // priest
+		'', // knight
+	],
+}
